078c2effdb4e7cf9d1034ddcc137c47a0535a68c
hatkar <hatmani_karim@hotmail.com>
2023-05-02 01:46:03 +0100

    restructuration de project

--- registry-service/src/main/java/com/hatmani/registryservice/RegistryServiceApplication.java
+++ registry-service/src/main/java/com/hatmani/registryservice/RegistryServiceApplication.java
@@ -1,5 +1,6 @@
 package com.hatmani.registryservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -8,7 +9,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class RegistryServiceApplication {
 
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(RegistryServiceApplication.class, args);
 	}
 
--- videostreamingsys/src/main/java/com/hatmani/videostreamingsys/VideostreamingsysApplication.java
+++ videostreamingsys/src/main/java/com/hatmani/videostreamingsys/VideostreamingsysApplication.java
@@ -1,9 +1,11 @@
 package com.hatmani.videostreamingsys;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 
 //@EnableConfigurationProperties({FileStorageProperties.class})
@@ -11,19 +13,25 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class VideostreamingsysApplication {
-
+    @Value("${spring.data.mongodb.database}")
+    String profile;
   /*  @Autowired
     CategorieService categorieService;
     @Autowired
     MovieService movieService;*/
 
+
     public static void main(String[] args) {
         SpringApplication.run(VideostreamingsysApplication.class, args);
-    }
 
-   /* private void init()
+    }
+@Bean
+     void init()
     {
-        System.out.println("Creation des Categories");
+        System.out.println("profile mongodb :"+profile);
+
+
+   /*     System.out.println("Creation des Categories");
         categorieService.saveCategorie(Mono.just(new CategoryDto(null,"Action","Last Action Movie",null)));
         categorieService.saveCategorie(Mono.just(new CategoryDto(null,"Romance","Last Action Movie",null)));
         categorieService.saveCategorie(Mono.just(new CategoryDto(null,"Horro","Last Action Movie",null)));
@@ -33,7 +41,7 @@ public class VideostreamingsysApplication {
         //affichage des movies
         System.out.println("list Categories");
         categorieService.getAllCategory().toStream().forEach(categoryDto -> System.out.println(categoryDto.getName()));//.map(c-> System.out.println(c.getName()));
-
-    }
 */
+    }
+
 }
